5d5d8d8fda1739951a67d6fe1564bea647008729
Mariluz Calderon <calderon.mariluz.8892@eam.edu.co>
2025-04-18 23:42:14 -0500

    ADD entity models

---
+++ src/app/models/share-split.model.ts
@@ -0,0 +1,9 @@
+export interface ShareSplit {
+  id_split: number;
+  id_share: number;
+  id_user: number;
+  percentage: number;
+  assigned_amount: number;
+  paid: number;
+  balance: number;
+}
